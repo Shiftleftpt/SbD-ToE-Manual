adfe2fa95f9f3a08e0909dee73c8254d1296772e
Pedro Farinha <pedro.farinha@shiftleft.pt>
2025-11-05 19:58:09 +0000

    fix manual link for domain securitybydesign

--- manuals_src/src/pages/index.tsx
+++ manuals_src/src/pages/index.tsx
@@ -7,7 +7,7 @@ import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
-  const introUrl = 'sbd-toe/sbd-manual/intro';
+  const introUrl = 'sbd-toe/sbd-manual/';
 
   return (
     <header className={clsx('hero', 'hero--primary', styles.slHero)}>
